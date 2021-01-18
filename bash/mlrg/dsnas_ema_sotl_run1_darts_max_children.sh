9673d027570c40e1a8821d38b6cbfa5c2d629ee8
Robin Ru <robin@robots.ox.ac.uk>
2021-01-18 15:03:12 +0800

    added in bash scripts

---
+++ bash/mlrg/dsnas_ema_sotl_run1_darts_max_children.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+    ##############################################################
+    
+    ##############################################################
+    
+# set the number of nodes
+#SBATCH --partition=small
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set the number of GPU cards to use per node
+#SBATCH --gres=gpu:1
+
+# set max wallclock time
+#SBATCH --time=72:00:00
+
+# set name of job
+#SBATCH --job-name=dsemasotldarts2
+#SBATCH --output=dsemasotldarts2.out
+
+# mail alert at start and end of execution
+#SBATCH --mail-type=ALL
+
+cd /jmain01/home/JAD017/sjr02/rxr89-sjr02/SNAS-SeriesDarts/SNAS/
+module load python3/anaconda
+source activate gpy
+cd /jmain01/home/JAD017/sjr02/rxr89-sjr02/SNAS-SeriesDarts/SNAS/
+
+
+# run the application
+python train_search.py --dsnas --epochs 150 --seed 6 --layer 8 --init_channels 16 --temp 1 \
+--temp_min 0.03 --nsample 1 --temp_annealing --resource_efficient --gen_max_child --alter_metric  "ema_sotl" \
+--resource_lambda 1.5e-3 --log_penalty --drop_path_prob 3e-1 --method 'reparametrization' \
+--loss --remark "dsnas_emasotl_darts_layer_8_batch_64_drop_0.3_error_lnR_1e-2_reparam_gpu_1"
+        
---
+++ bash/mlrg/eval/eval_e100_snas_run2_darts_max_children.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+    ##############################################################
+    
+    ##############################################################
+    
+# set the number of nodes
+#SBATCH --partition=small
+
+# set the number of nodes
+#SBATCH --nodes=1
+
+# set the number of GPU cards to use per node
+#SBATCH --gres=gpu:1
+
+# set max wallclock time
+#SBATCH --time=72:00:00
+
+# set name of job
+#SBATCH --job-name=evaldarts2e100
+#SBATCH --output=evaldarts2e100.out
+
+# mail alert at start and end of execution
+#SBATCH --mail-type=ALL
+
+cd /jmain01/home/JAD017/sjr02/rxr89-sjr02/SNAS-SeriesDarts/SNAS/
+module load python3/anaconda
+source activate gpy
+cd /jmain01/home/JAD017/sjr02/rxr89-sjr02/SNAS-SeriesDarts/SNAS/
+
+
+# run the application
+python train.py --auxiliary --cutout --seed 7 --arch 'snas_darts_run2_epoch100'
+
